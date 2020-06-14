c4eecd2d34e6fbf3cb8bc7c7cb9af9f8a32c69e3
Basko-Vladimir <1989bvg@gmail.com>
2020-06-14 18:41:06 +0300

    testing request headers

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 let smtp_login = process.env.SMTP_LOGIN;
 let smtp_password = process.env.SMTP_PASSWORD;
 

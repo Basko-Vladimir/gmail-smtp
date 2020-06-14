369f576b4fb02688fde42368cf8c7aa5bb72683d
Basko-Vladimir <1989bvg@gmail.com>
2020-06-14 13:35:40 +0300

    First commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: '1989bvg@gmail.com',
+        pass: "Bas'ko_190389",
+    },
+});
+
+app.post('/sendMessage', async function (req, res) {
+    const {message, name, email} = req.body;
+    await transporter.sendMail({
+        from: `HR\'s message trough my portfolio`,
+        to: '1989bvg@gmail.com',
+        subject: `HR's message trough my portfolio`,
+        // text: "Hello world?",
+        html: `<div>Name: ${name}</div>
+               <div>E-mail: ${email}</div>
+               <div>Message: ${message}</div>`
+    });
+
+    res.send(`Message sent!`)
+});
+
+app.listen(3010, function () {
+    console.log('Example app listening on port 3000!');
+});
